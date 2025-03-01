21e631c755d2aa4fb877ee417d8345784cce4f3f
avilesalejandro <great.solutions.dev@gmail.com>
2025-03-01 09:38:44 -0600

    Adaptador netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import netlify from '@astrojs/netlify';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+
+  output: 'server', 
+  integrations: [netlify()],
   vite: {
     plugins: [tailwindcss()]
   }
